f5b3e2d99694bee11e3bd2608b625fb61aedada2
Julle Kallio <julle.kallio@aalto.fi>
2024-07-18 13:07:44 +0000

    copilot 2nd commit

---
+++ member.js
@@ -0,0 +1,65 @@
+function skillsMember() {
+    if (document.getElementById("memberSkills")) {
+        var memberSkills = document.getElementById("memberSkills");
+        memberSkills.innerHTML = "";
+        memberSkills.innerHTML = `<div class="skill">
+        <div class="skill__header">
+            <h3>HTML</h3>
+            <span>90%</span>
+        </div>
+        <div class="skill__bar">
+            <span style="width: 90%;"></span>
+        </div>
+    </div>
+    <div class="skill">
+        <div class="skill__header">
+            <h3>CSS</h3>
+            <span>90%</span>
+        </div>
+        <div class="skill__bar">
+            <span style="width: 90%;"></span>
+        </div>
+    </div>
+    <div class="skill">
+        <div class="skill__header">
+            <h3>JavaScript</h3>
+            <span>80%</span>
+        </div>
+        <div class="skill__bar">
+            <span style="width: 80%;"></span>
+        </div>
+    </div>
+    <div class="skill">
+        <div class="skill__header">
+            <h3>React.js</h3>
+            <span>70%</span>
+        </div>
+        <div class="skill__bar">
+            <span style="width: 70%;"></span>
+        </div>
+    </div>
+    <div class="skill">
+        <div class="skill__header">
+            <h3>Node.js</h3>
+            <span>70%</span>
+        </div>
+        <div class="skill__bar">
+            <span style="width: 70%;"></span>
+        </div>
+    </div>
+    <div class="skill">
+        <div class="skill__header">
+            <h3>Express.js</h3>
+            <span>70%</span>
+        </div>
+        <div class="skill__bar">
+            <span style="width: 70%;"></span>
+        </div>
+    </div>
+    <div class="skill">
+        <div class="skill__header">
+            <h3>MongoDB</h3>
+            <span>70%</span>
+        </div>
+        <div class="skill__bar">
+            <span style="width:
